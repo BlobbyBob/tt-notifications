fadecd545adbaf023309d13acc31d923114ae30a
Ben Swierzy <swierzy@uni-bonn.de>
2023-03-09 23:28:31 +0100

    Added missing service worker update

--- frontend/public/service-worker.js
+++ frontend/public/service-worker.js
@@ -3,10 +3,27 @@ self.addEventListener('push', ev => {
     if (payload.msg) {
         ev.waitUntil(
             self.registration.showNotification("TT Benachrichtigung", {
-                body: payload.msg
+                body: payload.msg,
+                data: {
+                    id: payload.id,
+                    hasReport: payload.hasReport
+                }
             })
         );
     } else {
         console.warn("Received payload without message", ev.data);
     }
 });
+
+self.addEventListener("notificationclick", ev => {
+    ev.notification.close();
+
+    const id = ev.notification.data.id;
+    const hasReport = ev.notification.data.hasReport;
+    if (hasReport && id)
+        ev.waitUntil(
+            self.clients.openWindow(`${self.location.origin}/redirect-report/${id}`).then(
+                clientWindow => clientWindow ? clientWindow.focus() : undefined
+            )
+        );
+})
